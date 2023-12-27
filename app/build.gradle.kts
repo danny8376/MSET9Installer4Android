71fb5d0249b194b810402541f727207fc2eabbb1
DannyAAM <danny@saru.moe>
2023-12-27 13:54:13 +0800

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "moe.saru.homebrew.console3ds.mset9_installer_android"
         minSdk = 21
         targetSdk = 33
-        versionCode = 9
-        versionName = "1.1"
+        versionCode = 10
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
